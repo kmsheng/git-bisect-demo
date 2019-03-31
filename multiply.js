d6b143d4d8e03a31110689dc8131c4447675ef03
kmsheng <kmsh3ng@gmail.com>
2019-03-31 19:05:13 +0800

    feat: add file multiply.js

---
+++ multiply.js
@@ -0,0 +1,9 @@
+/**
+ * Just a simple function to multiply two numbers together.
+ *
+ * @param {a} first number
+ * @param {b} second number
+ */
+module.exports = function multiply(a, b) {
+  return a * b;
+};
